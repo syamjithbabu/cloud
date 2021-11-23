33c0fa4aabfad81bbbd4c555923f85b875aeaba0
syamjithbabu <syamjithbabu2000@gamil.com>
2021-11-23 12:38:13 +0530

    version1

---
+++ high/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class HighConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'high'
---
+++ high/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def Down(request):
+    return render(request, 'for.html')
