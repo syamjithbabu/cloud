cde88f1ac3b589896cf4d450442a1d96a35985b8
syamjithbabu <syamjithbabu2000@gamil.com>
2021-11-23 13:28:21 +0530

    ver3

--- project5/settings.py
+++ project5/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -121,9 +122,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS =[
+    os.path.join(BASE_DIR,'high/static')
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_STORAGE = 'whitenoise.storageCompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storageCompressedManifestStaticFilesStorage'
